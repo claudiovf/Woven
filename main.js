16389d3c87006d0cd63ebe2f23619074f1cc3ed5
Claudio <claudiovf@hotmail.com>
2020-06-02 00:12:52 +1000

    added search icon

--- main.js
+++ main.js
@@ -38,6 +38,26 @@ $(window).scroll(function() {
     })
  
 
+
+    $('.icon-search').on('click', function(event) {
+        if (!$('.top-line').hasClass('top-line-active')) {
+            $('.top-line').addClass('top-line-active');
+            $('.search').css("background-image", "none");
+            $('.search').css("background-color", "rgba(255, 255, 255, 0.955)");
+        }else{
+            $('.top-line').removeClass('top-line-active');
+            $('.search').css("background-image", "url(images/magnifier.png)");
+            $('.search').css("background-color", "none");
+        }
+    })
+    $('.icon-search').on('click', function(event) {
+        if (!$('.bottom-line').hasClass('bottom-line-active')) {
+            $('.bottom-line').addClass('bottom-line-active');
+        }else{
+            $('.bottom-line').removeClass('bottom-line-active');
+        }
+    })
+
   const mainMenu = document.getElementById('main-menu');
   let overlay = document.getElementById('overlay');
   let menuOverlay = document.getElementById('menuOverlay');
@@ -46,15 +66,27 @@ $(window).scroll(function() {
 
 
   function toggleMenu() {
+      if(searchOverlay.classList.contains('active-search')) {
+          searchOverlay.classList.remove('active-search');
+          overlay.classList.remove('active2');
+          overlay.classList.toggle('active');
+          menuOverlay.classList.toggle('active-menu');
+      }else{
     overlay.classList.toggle('active');
     menuOverlay.classList.toggle('active-menu');
     console.log('hello');
-  }
+  }}
 
   function toggleSearch() {
+      if(menuOverlay.classList.contains('active-menu')) {
+          menuOverlay.classList.remove('active-menu');
+          overlay.classList.remove('active');
+          overlay.classList.toggle('active2');
+          searchOverlay.classList.toggle('active-search');
+      }else{
       overlay.classList.toggle('active2');
       searchOverlay.classList.toggle('active-search');
-
+      }
   }
 
   mainMenu.addEventListener('click', toggleMenu);
